6b47448d97ebd98f656cfc7bd3364e8c09f57e34
Monk191173 <evg_evg@ukr.net>
2023-08-22 19:37:16 +0300

    ang01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+import { CensorComponent } from './censor/censor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CensorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CensorComponent]
 })
 export class AppModule { }
